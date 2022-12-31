29412be252f725ed1e84bd3dcbebdeb3467fc061
blandh26 <blandh26@gmail.com>
2022-12-31 22:36:25 +0800

    截图类库第一次提交

---
+++ H.ScreenCapture/ColorBoxDesginer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Windows.Forms.Design;
+
+namespace H.ScreenCapture
+{
+    public class ColorBoxDesginer : ControlDesigner
+    {
+        public override SelectionRules SelectionRules {
+            get {
+                return base.SelectionRules & ~SelectionRules.AllSizeable;
+            }
+        }
+    }
+}
--- H.ScreenCapture/ScreenCapture.cs
+++ H.ScreenCapture/ScreenCapture.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace H.ScreenCapture
 {
-    public class Class1
+    public class ScreenCapture
     {
     }
 }
